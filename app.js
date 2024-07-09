70cc5f7d9bfd5209b846ea5736e3f404cb4e4ddf
Santii05-C <santi.c.pgm@gmail.com>
2024-07-09 11:00:54 -0300

    last commit

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(
+  cors({ origin: "https://estate-app-xi.vercel.app/", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 
