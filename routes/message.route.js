65818ecf9595ccf3a31fe6df13a3565ae7f80ec5
Santii05-C <santi.c.pgm@gmail.com>
2024-07-11 11:25:48 -0300

    last commit

--- routes/message.route.js
+++ routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { addMessage } from "../controllers/message.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
+// import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/:chatId", verifyToken, addMessage);
+// router.post("/:chatId", verifyToken, addMessage);
+router.post("/:chatId", addMessage);
 
 export default router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -8,16 +8,21 @@ import {
   profilePosts,
   getNotificationNumber,
 } from "../controllers/user.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
+// import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 // router.get("/search/:id", verifyToken, getUser);
-router.put("/:id", verifyToken, updateUser);
-router.delete("/:id", verifyToken, deleteUser);
-router.post("/save", verifyToken, savePost);
-router.get("/profilePosts", verifyToken, profilePosts);
-router.get("/notification", verifyToken, getNotificationNumber);
+// router.put("/:id", verifyToken, updateUser);
+// router.delete("/:id", verifyToken, deleteUser);
+// router.post("/save", verifyToken, savePost);
+// router.get("/profilePosts", verifyToken, profilePosts);
+// router.get("/notification", verifyToken, getNotificationNumber);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.post("/save", savePost);
+router.get("/profilePosts", profilePosts);
+router.get("/notification", getNotificationNumber);
 
 export default router;
