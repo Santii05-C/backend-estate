96f29f3509254d41d28cfc51e0af03e000cf231c
Santii05-C <santi.c.pgm@gmail.com>
2024-07-11 11:15:13 -0300

    last commit

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,13 +1,22 @@
 import express from "express";
-import {verifyToken} from "../middleware/verifyToken.js";
-import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
+// import {verifyToken} from "../middleware/verifyToken.js";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
-router.post("/", verifyToken, addPost);
-router.put("/:id", verifyToken, updatePost);
-router.delete("/:id", verifyToken, deletePost);
+// router.post("/", verifyToken, addPost);
+// router.put("/:id", verifyToken, updatePost);
+// router.delete("/:id", verifyToken, deletePost);
+router.post("/", addPost);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
